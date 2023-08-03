1caa4d77159248c280c0bf1394ccf844c11d437f
kimGyeongHa <rudgkrk11@naver.com>
2023-08-03 14:37:11 +0900

    doll_drwoing resolve

--- src/dollDrawing.java
+++ src/dollDrawing.java
@@ -2,57 +2,50 @@ import java.util.*;
 
 public class dollDrawing {
 
-    static int drowing_doll_count(List<Queue<Integer>> drawing_machine, int[] drawing_pick){
-       int result = 0;
-       Stack<Integer> stack = new Stack<>();
+    static int remove_doll_count(List<Queue<Integer>> drawing_machine, int[] drawing_pick){
+        int result = 0;
+        Stack<Integer> stack_list = new Stack<>();
 
-       for(int pick : drawing_pick){
-           pick = pick != 0 ? pick-1 : pick;
-           Queue<Integer> machine_num =  drawing_machine.get(pick);
-           if(!machine_num.isEmpty()) stack.add(machine_num.poll());
-       }
-
-       int compare_num = stack.get(0);
-
-       for(int i = 1 ; i < stack.size() ; i++){
-           if(stack.get(i) == compare_num){
-               result++;
-               compare_num = stack.get(i);
-           }
-       }
-
+        // 각 열의 값에서 뽑은 값을 stack으로 이동하는 로직
+        for(int pick : drawing_pick){
+            Queue<Integer> machine_num = drawing_machine.get(pick-1);
+            if(!machine_num.isEmpty()) {
+                int poll_num = machine_num.poll();
+                if(stack_list.size() > 1 && stack_list.peek() == poll_num){
+                    result += 2;
+                    stack_list.remove(stack_list.peek());
+                }else stack_list.add(poll_num);
+            }
+        }
        return result;
     }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int stack_size = sc.nextInt();
+        int arr_size = sc.nextInt();
         List<Queue<Integer>> stack_list = new LinkedList<>();
         Queue<Integer> stack_values = null;
 
-        for(int i=0 ; i< stack_size ; i++){
-            stack_values = new LinkedList<>();
-            for(int j = 0 ; j < stack_size ; j++){
-                int zero_chk = sc.nextInt();
-                if(zero_chk != 0 ) stack_values.add(zero_chk);
-            }
-            stack_list.add(stack_values);
-        }
-
-        /*
-
+        // 이중배열에 값 담기
         int[][] drawing_machine = new int[arr_size][arr_size];
 
-        for(int i=0 ; i< drawing_machine.length; i++){
-            for(int j=0 ; j < drawing_machine[i].length ; j++){
+        for(int i=0 ; i < drawing_machine.length; i++){
+            for(int j=0 ; j < drawing_machine[0].length ; j++){
                 drawing_machine[i][j] = sc.nextInt();
             }
         }
-        */
 
+        // 이중배열의 각 열의 값들을 stack에 넣기
+        int arrCnt = 0;
+        for(int i= 0 ; i < arr_size ; i++){
+            stack_values = new ArrayDeque<>();
+            for(int j = 0 ; j < arr_size ; j++) if(drawing_machine[j][arrCnt] != 0) stack_values.add(drawing_machine[j][arrCnt]);
+            arrCnt++;
+            stack_list.add(stack_values);
+        }
 
+        // 사용자 움직임 배열
         int drawing_pick_size = sc.nextInt();
         int[] drawing_pick = new int[drawing_pick_size];
 
@@ -60,7 +53,7 @@ public class dollDrawing {
             drawing_pick[i] = sc.nextInt();
         }
 
-        System.out.println(drowing_doll_count(stack_list, drawing_pick));
+        System.out.println(remove_doll_count(stack_list, drawing_pick));
 
     }
 }
